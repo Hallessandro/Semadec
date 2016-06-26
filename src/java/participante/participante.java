fda24f82ea58f2ef6229ad76bf3e2eb59db72013
Hallessandro <hd.jesus93@gmail.com>
2016-06-26 20:10:16 -0300

    Commit inicial

---
+++ src/java/participante/participante.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package participante;
+
+/**
+ *
+ * @author Hallessandro
+ */
+public class participante {
+
+    String nome; 
+    String matricula; 
+
+    public participante() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+    
+}
